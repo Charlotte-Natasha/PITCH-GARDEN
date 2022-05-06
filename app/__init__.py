886f8b9d08ddb70b666b569f08f18518b96e4b8f
Charlotte Natasha <natashaonyango97@gmail.com>
2022-05-06 20:39:56 +0200

    installed SQL and flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+from app.config import DevConfig
 
+# Initializing Application
 app = Flask(__name__)
 
-from app import views
+# Setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+from .views import views
+app.register_blueprint(views, url_prefix="/")
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from flask import render_template
+from flask import Blueprint
 from app import app
+
+views = Blueprint("views", __name__)
 
 #Views
 @app.route('/')
