c83be820a3986b6461bdac5b6d2f87dc639a97b7
Charlotte Natasha <natashaonyango97@gmail.com>
2022-05-06 21:20:03 +0200

    added login and signup files

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from os import path
-from flask_login import LoginManager
-from app.config import DevConfig
+# from flask_login import LoginManager
+
 
 # Initializing Application
 app = Flask(__name__)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
 
 from .views import views
 app.register_blueprint(views, url_prefix="/")
