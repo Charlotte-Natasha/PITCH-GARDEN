a8ec323c74dd1364e8d6eb943a58c76d28f5e7c6
Charlotte Natasha <natashaonyango97@gmail.com>
2022-05-08 23:13:28 +0200

    imported flask_migrate

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,39 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from flask_migrate import Migrate
+import psycopg2 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Initializing Application
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__) 
+    DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user='natasha245',pw='tasha',url='localhost',db='pitch')
+    
+    app.config['SECRET_KEY'] = 'charlotte'
+    app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
+
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
-    
+
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+
+
+
+
+
+
+
     return app
+    
     
 
---
+++ instance/config.py
@@ -0,0 +1,1 @@
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user='natasha245',pw='tasha',url='localhost',db='pitch')
